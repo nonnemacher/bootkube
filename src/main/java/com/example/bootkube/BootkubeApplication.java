763fb5e4a9ccd0e6f15533b504a8d09e62fa8053
Carlos Henrique Nonnemacher <carloshenrique@cwi.com.br>
2017-11-15 01:27:00 -0200

    init

---
+++ src/main/java/com/example/bootkube/BootkubeApplication.java
@@ -0,0 +1,12 @@
+package com.example.bootkube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootkubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootkubeApplication.class, args);
+	}
+}
